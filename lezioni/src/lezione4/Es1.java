89a1fae308d0cf08633db20e0c0a4cb07b141a02
agaamma <agaamma@gmail.com>
2024-02-13 16:55:45 +0100

    pomeriggio martedi 13 feb

--- lezioni/src/lezione4/Es1.java
+++ lezioni/src/lezione4/Es1.java
@@ -1,4 +1,9 @@
 package lezione4;
 
 public class Es1 {
+    public static void main(String[] args) {
+        //Esercizio 3 (for-if): quanto è la somma dei numeri da 1 a 100 pari o divisibili per 5?
+        int n1, n2;
+        for (int k = 1; k <= 100; k+= 2);
+    }
 }
---
+++ lezioni/src/lezione4/Es4.java
@@ -0,0 +1,7 @@
+package lezione4;
+
+public class Es4 {
+    public static void main(String[] args) {
+
+    }
+}
